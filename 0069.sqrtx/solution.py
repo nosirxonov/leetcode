878caffe83c1b586963eb2ae33fcd15207e485c1
nosirxonov <jonibeknosirxonov0@gmail.com>
2026-09-21 14:37:09 +0500

    0069

--- 0069.sqrtx/solution.py
+++ 0069.sqrtx/solution.py
@@ -9,7 +9,24 @@ from leetgo_py import *
 
 class Solution:
     def mySqrt(self, x: int) -> int:
-        return int(x ** 0.5)
+        if x == 0:
+            return 0
+            
+        left, right = 1, x
+        ans = 0
+        
+        while left <= right:
+            mid = (left + right) // 2
+            
+            if mid * mid == x:
+                return mid
+            elif mid * mid < x:
+                ans = mid
+                left = mid + 1
+            else:
+                right = mid - 1
+                
+        return ans
 
 # @lc code=end
 
